3c189f7e85cf6f8a9494250881e231a11f78afff
shota.hasegawa <pphasesoccerv@gmail.com>
2020-08-23 08:45:55 +0000

    user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,6 @@ class UsersController < ApplicationController
   
   def likes
     @user = User.find(params[:id])
-    @likes = @user.likes.page(params[:page])
     @microposts = @user.likes.order(id: :desc).page(params[:page])
     counts(@user)
   end
